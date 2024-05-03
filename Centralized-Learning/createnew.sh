3e13db67c8c4af79a30936ddce3f0a067591ba1f
huihui0310tt <huihui0310tt@gmail.com>
2024-05-03 20:51:28 +0800

    update CL
    add custom CL data size

--- Centralized-Learning/createnew.sh
+++ Centralized-Learning/createnew.sh
@@ -4,7 +4,7 @@ if [ -d "data" ]; then
     rm -r "data"
 fi
 
-user_names="centralizedlearningdata"
+user_names=($(jq -r '.User[]' configure.json))
 classes=($(jq -r '.DataCategory[]' configure.json))
 
 for user in "${user_names[@]}"; do
@@ -13,30 +13,22 @@ for user in "${user_names[@]}"; do
     done
 done
 
+# shared
+for class in "${classes[@]}"; do
+	mkdir -p "data/shared/${class}"
+done
+
+python ./1-classification.py
+
 DatasetName=($(jq -r '.DatasetName' configure.json))
 
 if [ "$DatasetName" = "COVID" ]; then
-    cp -r ../COVID_origindata/COVID 			./data/centralizedlearningdata
-	cp -r ../COVID_origindata/Lung_Opacity 		./data/centralizedlearningdata
-    cp -r ../COVID_origindata/Normal 			./data/centralizedlearningdata
-    cp -r ../COVID_origindata/VirtualPneumonia 	./data/centralizedlearningdata
-
     cp -r ../COVID_origindata/test ./data
 elif [ "$DatasetName" = "CIFAR" ]; then
-    cp -r ../CIFAR_origindata/0_airplane 	./data/centralizedlearningdata
-	cp -r ../CIFAR_origindata/1_automobile 	./data/centralizedlearningdata
-    cp -r ../CIFAR_origindata/2_bird 		./data/centralizedlearningdata
-    cp -r ../CIFAR_origindata/3_cat 		./data/centralizedlearningdata
-    cp -r ../CIFAR_origindata/4_deer 		./data/centralizedlearningdata
-	cp -r ../CIFAR_origindata/5_dog 		./data/centralizedlearningdata
-    cp -r ../CIFAR_origindata/6_frog 		./data/centralizedlearningdata
-    cp -r ../CIFAR_origindata/7_horse 		./data/centralizedlearningdata
-    cp -r ../CIFAR_origindata/8_ship 		./data/centralizedlearningdata
-	cp -r ../CIFAR_origindata/9_truck 		./data/centralizedlearningdata
-    cp -r ../CIFAR_origindata/test 			./data
+    cp -r ../CIFAR_origindata/test ./data
 else
     echo "DatasetName is neither 'COVID' nor 'CIFAR'"
 fi
 
 
-python ./2-getinfo.py "$user_names" "$classes_string"
+python ./2-getinfo.py "$user_names_string" "$classes_string"
